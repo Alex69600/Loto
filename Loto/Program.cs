aeacfb1c1e70d8aef41ccba8042d45286f3d088d
Alexandre <ALEX@ALEX_PC>
2014-03-25 16:59:25 +0100

    Commit Loto

---
+++ Loto/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tabLoto= new int[6];
+		    Random r=new Random();
+		    DateTime date = DateTime.Now;
+		
+		
+
+
+		Console.WriteLine("--------------------------------------------- \n");
+		Console.Write("\n *    ****         *    **** ***** ****");
+		Console.Write("\n *    ***          *    *  *   *   *  *");
+		Console.Write("\n **** ****         **** ****   *   ****");
+		Console.Write("\n tirage du "+ date.Day +"/"+ date.Month +"/"+ date.Year +" a "+ date.Hour +"h"+ date.Minute);
+
+
+		for (int i=0;i<6;i++)
+		{
+			tabLoto[i]=Math.Abs(r.Next(50));
+		}
+
+		Console.Write("\n Les 6 chiffres du loto dans l'ordre du tirage");
+
+		for(int i=0;i<6;i++)
+		{
+			Console.Write("\n    "+tabLoto[i]);
+		}
+
+        Array.Sort(tabLoto);
+		Console.Write("\n Les 6 chiffres tries");
+
+		for(int i=0;i<6;i++)
+		{
+			Console.Write("\n    "+tabLoto[i]);
+		}
+
+		Console.Write("\n ---------------------------------------------");
+        Console.ReadLine();
+        }
+      
+    }
+}
